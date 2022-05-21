c69f6b0bd9d748a4f0e4964ae81784dc4064d4cd
Roberto Arruda <betoakang@hotmail.com>
2022-05-21 02:58:55 -0300

    feat: ensure SignupController returns 400 if an invalid email is provided

--- src/controllers/signup/signup.js
+++ src/controllers/signup/signup.js
@@ -1,13 +1,22 @@
 const { badRequest } = require('../../helpers/http/http-helper')
-const { MissingParamError } = require('../../errors')
+const { MissingParamError, InvalidParamError } = require('../../errors')
 
 class SingupController {
+  constructor (emailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest) {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
--- src/errors/index.js
+++ src/errors/index.js
@@ -1,2 +1,3 @@
 exports.ServerError = require('./server-error')
 exports.MissingParamError = require('./missing-param-error')
+exports.InvalidParamError = require('./invalid-param-error')
