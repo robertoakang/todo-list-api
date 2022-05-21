8c229c6274dc6078488ccbc4a82469daf0c4b0ec
Roberto Arruda <betoakang@hotmail.com>
2022-05-21 05:30:56 -0300

    test: ensure EmailValidatorAdapter works correctly

---
+++ src/utils/email-validator-adapter.spec.js
@@ -0,0 +1,32 @@
+const EmailValidatorAdapter = require('./email-validator-adapter')
+const validator = require('validator')
+
+jest.mock('validator', () => ({
+  isEmail () {
+    return true
+  }
+}))
+
+const makeSut = () => new EmailValidatorAdapter()
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail .com')
+    expect(isValid).toBe(false)
+  })
+
+  test('Should return false if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
