6bc247ec623e81f380a7b7cc69edce8fc3188386
Roberto Arruda <betoakang@hotmail.com>
2022-05-21 14:51:19 -0300

    test: add new route to LoginController

--- src/server/routes/routes.test.js
+++ src/server/routes/routes.test.js
@@ -4,6 +4,7 @@ const request = require('supertest')
 const app = require('../app')
 
 describe('SignUp Routes', () => {
+  let payloadAndToken = ''
   const mongoHandler = new MongoHandler(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -36,11 +37,28 @@ describe('SignUp Routes', () => {
   })
 
   test('Should return the tokens on success', async () => {
-    await request(app)
+    const result = await request(app)
       .post('/api/v1/user/login')
       .send({
         email: 'betoakang@gmail.com',
         password: '123'
+      })
+
+    payloadAndToken = result._body
+    expect(result.statusCode).toBe(200)
+  })
+
+  test('Should return the token and payload on success', async () => {
+    console.log(payloadAndToken)
+    await request(app)
+      .post('/api/v1/user/refresh')
+      .send({
+        payload: {
+          id: 'valid_id',
+          name: 'Roberto',
+          email: 'betoakang@gmail.com'
+        },
+        refreshToken: payloadAndToken.refreshToken
       })
       .expect(200)
   })
