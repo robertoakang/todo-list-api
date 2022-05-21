19578515a37c2c9ea1d15ce18e33dd886a5a14b5
Roberto Arruda <betoakang@hotmail.com>
2022-05-21 04:55:25 -0300

    test: add Express config

---
+++ src/server/middlewares/middleware.test.js
@@ -0,0 +1,48 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('Middlewares', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'Roberto' })
+      .expect({ name: 'Roberto' })
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+
+  test('Should return default content type as json', async () => {
+    app.get('/test_content_type', (req, res) => {
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
+})
