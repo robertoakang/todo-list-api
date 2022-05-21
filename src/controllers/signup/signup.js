43adf86fba5f7f0cd23dc04c2782a43012b182d6
Roberto Arruda <betoakang@hotmail.com>
2022-05-21 03:53:10 -0300

    feat: ensure SignupController calls AddAccountService with correct values

--- src/controllers/signup/signup.js
+++ src/controllers/signup/signup.js
@@ -2,8 +2,9 @@ const { badRequest, serverError } = require('../../helpers/http/http-helper')
 const { MissingParamError, InvalidParamError } = require('../../errors')
 
 class SingupController {
-  constructor (emailValidator) {
+  constructor (emailValidator, accountService) {
     this.emailValidator = emailValidator
+    this.accountService = accountService
   }
 
   async handle (httpRequest) {
@@ -15,14 +16,19 @@ class SingupController {
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      const { name, email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      const account = this.accountService.add({ name, email, password })
+      return account
     } catch (error) {
       console.error(error)
       return serverError()
